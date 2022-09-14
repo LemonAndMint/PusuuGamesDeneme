f17222d1519e7909cc81bfac5b028d2cc15a4d03
akovetv <68794752+akovetv@users.noreply.github.com>
2022-09-15 00:41:31 +0300

    sa

---
+++ Assets/Scripts/movement/Controller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+
+    void Update()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        gameObject.transform.position = new Vector3(x * 4, y * 4, transform.position.z);
+
+    }
+}
