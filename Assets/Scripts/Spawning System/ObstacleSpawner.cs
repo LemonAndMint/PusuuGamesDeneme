f6040bb4ba9769e9ceb545eb803443225eeb1a15
ROWLEYES\donat <bahattin.ercan.43@gmail.com>
2022-09-14 22:53:34 +0300

    ObstacleSpawner1.1

--- Assets/Scripts/Spawning System/ObstacleSpawner.cs
+++ Assets/Scripts/Spawning System/ObstacleSpawner.cs
@@ -4,9 +4,12 @@ public class ObstacleSpawner : MonoBehaviour
 {
     public static ObstacleSpawner Instance;
     [SerializeField] private float spawnDelay;
+    [SerializeField] private float decreaseSpawnDelay;
+    [SerializeField,Range(0,100)] private float decreaseSpawnDistancePercent;
     [SerializeField] private Transform obsPrefab;
     [SerializeField] private Vector3 baseSpawnPos;
     [SerializeField] private float spawnDistance;
+    private float baseSpawnDistance;
     [SerializeField] private Transform player;
     private void Awake()
     {
@@ -15,7 +18,14 @@ public class ObstacleSpawner : MonoBehaviour
 
     private void Start()
     {
+        baseSpawnDistance = spawnDistance;
         StartSpawn();
+        InvokeRepeating("DecreaseSpawnDistance", decreaseSpawnDelay, decreaseSpawnDelay);        
+    }
+
+    private void DecreaseSpawnDistance()
+    {
+        spawnDistance = spawnDistance - (baseSpawnDistance * decreaseSpawnDistancePercent / 100);
     }
 
     private void SpawnObstacle()
@@ -23,17 +33,18 @@ public class ObstacleSpawner : MonoBehaviour
         Vector3 spawnPos = new Vector3(baseSpawnPos.x, baseSpawnPos.y, baseSpawnPos.z+spawnDistance+player.transform.position.z);
         Transform obstacleHolder = Instantiate(obsPrefab.transform, spawnPos,Quaternion.identity);
         int random = Random.Range(1, 10);
-        Debug.Log(random);
-        obstacleHolder.Find(random.ToString()).gameObject.SetActive(false);
+        obstacleHolder.Find(random.ToString()).gameObject.SetActive(false);        
     }
 
     public void StartSpawn()
     {
         InvokeRepeating("SpawnObstacle", spawnDelay, spawnDelay);
+        InvokeRepeating("DecreaseSpawnDistance", decreaseSpawnDelay, decreaseSpawnDelay);
     }
 
     public void StopSpawn()
     {
         CancelInvoke("SpawnObstacle");
+        CancelInvoke("DecreaseSpawnDistance");
     }
 }
