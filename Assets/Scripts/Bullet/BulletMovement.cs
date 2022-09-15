65d7638eaceed922ea244e10f4aae001201be7ed
LemonAndMint <orn.betul@gmail.com>
2022-09-15 10:03:06 +0300

    Fixed bullet movement rotation limitation

--- Assets/Scripts/Bullet/BulletMovement.cs
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -41,13 +41,8 @@ public class BulletMovement : MonoBehaviour
             Quaternion tempRotationQuaternion = Quaternion.Euler( -1 * Input.GetAxisRaw("Vertical") * rotateSpeed * Time.deltaTime,
                                                                   Input.GetAxisRaw("Horizontal") * rotateSpeed * Time.deltaTime, 0 ) * rb.rotation;
 
-            /*Vector3 tempRotationVector3 = tempRotationQuaternion.eulerAngles; #FIXME 0 a gelince rotasyon degerleri max degerlere dönuyor / Corpyr
+            tempRotationQuaternion = _limitRotation(tempRotationQuaternion);
 
-            tempRotationQuaternion = Quaternion.Euler( Mathf.Clamp(tempRotationVector3.x, -1 * maxXRotate, maxXRotate),
-                                                       Mathf.Clamp(tempRotationVector3.y, -1 * maxYRotate, maxYRotate),
-                                                       0 );*/  
-
-            //rb.MoveRotation( rb.rotation * tempRotationQuaternion );
             rb.MoveRotation( tempRotationQuaternion );
 
             return;
@@ -55,5 +50,25 @@ public class BulletMovement : MonoBehaviour
         }
 
         rb.MoveRotation( Quaternion.Lerp(Quaternion.identity, rb.rotation, 0.993f) );
+    }
+
+    private Quaternion _limitRotation(Quaternion rotation){ // rotasyon alinir, vector 3 e cevrilir ve 180 derece degerine gore kalibre edilir.
+                                                            // sinirlamalar yapilir ve tekrardan quaterniona cevrilir. / Corpyr
+
+        Quaternion tempRotationQuaternion;
+        Vector3 tempRotationVector3 = rotation.eulerAngles;
+        
+        tempRotationVector3.x = (tempRotationVector3.x > 180) ? tempRotationVector3.x - 360 : tempRotationVector3.x;
+        tempRotationVector3.x = Mathf.Clamp(tempRotationVector3.x, -1 * maxXRotate, maxXRotate);
+
+        tempRotationVector3.y = (tempRotationVector3.y > 180) ? tempRotationVector3.y - 360 : tempRotationVector3.y;
+        tempRotationVector3.y = Mathf.Clamp(tempRotationVector3.y, -1 * maxXRotate, maxXRotate);
+
+        tempRotationVector3.z = 0f;
+
+        tempRotationQuaternion = Quaternion.Euler( tempRotationVector3 );
+
+        return tempRotationQuaternion;
+
     }
 }
